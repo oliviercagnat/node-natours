ef55d8a8aac354f677660a88b4aaf853f488324c
oliviercagnat <oliviercagnat@gmail.com>
2022-10-05 09:58:50 +0200

    add files

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(
     `${__dirname}/dev-data/data/tours-simple.json`
@@ -19,8 +24,10 @@ const tours = JSON.parse(
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours: tours,
